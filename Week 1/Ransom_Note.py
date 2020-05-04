3636f632a9500b90ded3625c6ff777e1630a38a6
Ankush2811 <ankushawsm@gmail.com>
2020-05-04 19:55:16 +0530

    Delete Ransom_Note.py

--- Week 1/Ransom_Note.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        for i in magazine:
-            if i in ransomNote:
-                magazine.remove(i)
-            else:
-                return False
-        return True
-        
